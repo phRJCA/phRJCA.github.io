d1fcb28835d05b286fdb5d37413a41a6fbc5d34e
RJCA <reynald@magiciantools.com>
2019-02-16 21:36:57 +0900

    itoigawa2019: New content

---
+++ js/itoigawa2019.js
@@ -0,0 +1,153 @@
+// Pic Urls
+// These pictures are ordered accordingly
+const itoigawaMapPics = [
+  '/img/itoigawa2019/itoigawa-map/itoigawa-map.png'
+];
+
+const itoigawaEnvPics = [
+  '/img/itoigawa2019/itoigawa-env/itoigawa-mae.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-env1.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-env2.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-env3.jpg',
+  '/img/itoigawa2019/itoigawa-env/itoigawa-road.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-env4.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-env5.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-env6.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-env7.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-road.jpg',
+  '/img/itoigawa2019/itoigawa-env/snow-torii.jpg',
+  '/img/itoigawa2019/itoigawa-env/itoigawa-umi.jpg'
+];
+
+const itoigawaYukiPics = [
+  '/img/itoigawa2019/itoigawa-yukikaki/hut.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/hut-group.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-act1.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-act2.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/hut2-group.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/hut2-group2.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-act3.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-act4.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-act5.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-act6.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-act7.jpg',
+  '/img/itoigawa2019/itoigawa-yukikaki/rjca-yukikaki-dojo.jpg'
+];
+
+const itoigawaHokaPics = [
+  '/img/itoigawa2019/itoigawa-hoka/itoigawa-np.jpg',
+  '/img/itoigawa2019/itoigawa-hoka/rjca-buried1.jpg',
+  '/img/itoigawa2019/itoigawa-hoka/rjca-buried2.jpg',
+  '/img/itoigawa2019/itoigawa-hoka/rjca-plow.jpg'
+];
+
+const imageCount = {
+  'itoigawa-map': itoigawaMapPics.length,
+  'itoigawa-env': itoigawaEnvPics.length,
+  'itoigawa-yukikaki': itoigawaYukiPics.length,
+  'itoigawa-hoka': itoigawaHokaPics.length
+};
+
+function setPicCount() {
+  const targetId = event.target.id;
+  const picCount = imageCount[targetId];
+  event.target.setAttribute("pic-count", picCount);
+
+  // Show next/previous image
+  if (picCount > 1) {
+    document.getElementById('img-highlight').innerHTML = (
+      "<div id='prev-img-con' class='img-container'>" +
+        "<i id='prev-img' class='fas fa-angle-left fa-2x icon-container' onclick='getPrevPic()'></i>" +
+      "</div>" +
+      "<div id='next-img-con' class='img-container'>" +
+        "<i id='next-img' class='fas fa-angle-right fa-2x icon-container' onclick='getNextPic()'></i>" +
+      "</div>" +
+      "<div id='img-counter'>Image: " + (currentPicIdx + 1) + "/" + picCount + "</div>"
+    );
+  } else {
+    document.getElementById('img-highlight').innerHTML = '';
+  };
+};
+
+function getPrevPic(e) {
+  let picCount = 0;
+  let picUrls = '';
+
+  // See main.js for more details on currentPicIdx & hoveredList
+  switch (hoveredList && hoveredList.id) {
+    // itoigawa-map only has 1 pic
+    case "itoigawa-env":
+      picCount = imageCount["itoigawa-env"];
+      picUrls = itoigawaEnvPics;
+      break;
+    case "itoigawa-yukikaki":
+      picCount = imageCount["itoigawa-yukikaki"];
+      picUrls = itoigawaYukiPics;
+      break;
+    case "itoigawa-hoka":
+      picCount = imageCount["itoigawa-hoka"];
+      picUrls = itoigawaHokaPics;
+      break;
+    default:
+      return;
+  };
+
+  currentPicIdx = getImgIdx(currentPicIdx, picCount, "prev");
+  // Update image counter
+  document.getElementById('img-counter').innerText = "Image: " + (currentPicIdx + 1) + "/" + picCount
+
+  const img = document.getElementById('img-highlight');
+  img.style.backgroundImage = "url(" + picUrls[currentPicIdx] + ")";
+};
+
+function getNextPic() {
+  let picCount = 0;
+  let picUrls = '';
+
+  // See main.js for more details on currentPicIdx & hoveredList
+  switch (hoveredList && hoveredList.id) {
+    // itoigawa-map only has 1 pic
+    case "itoigawa-env":
+      picCount = imageCount["itoigawa-env"];
+      picUrls = itoigawaEnvPics;
+      break;
+    case "itoigawa-yukikaki":
+      picCount = imageCount["itoigawa-yukikaki"];
+      picUrls = itoigawaYukiPics;
+      break;
+    case "itoigawa-hoka":
+      picCount = imageCount["itoigawa-hoka"];
+      picUrls = itoigawaHokaPics;
+      break;
+    default:
+      return;
+  };
+
+  currentPicIdx = getImgIdx(currentPicIdx, picCount, "next");
+  // Update image counter
+  document.getElementById('img-counter').innerText = "Image: " + (currentPicIdx + 1) + "/" + picCount
+
+  const img = document.getElementById('img-highlight');
+  img.style.backgroundImage = "url(" + picUrls[currentPicIdx] + ")";
+};
+
+function getImgIdx(currentIdx, imgCount, type) {
+  switch (type) {
+    case "prev":
+      currentIdx -= 1;
+      if (currentIdx < 0) {
+        currentIdx = imgCount - 1; // Get the last image
+      };
+      break;
+    case "next":
+      currentIdx += 1;
+      if (currentIdx > (imgCount - 1) ) {
+        currentIdx = 0; // Get the first image
+      };
+      break;
+    default:
+      // Do nothing
+  }
+
+  return currentIdx;
+};
--- js/main.js
+++ js/main.js
@@ -1,139 +1,19 @@
-const travelMapText = "<p>On the first few days, I planned my travels to look for some snow.</p>" +
-  "<p>And so I did.</p>" +
-  "<p>Then I thought: It might be more fun if I stopped planning and go with the flow.</p>" +
-  "<p>And so I did.</p>" +
-  "<br>" +
-  "<p>始めから、旅行は雪を見るために計画した。</p>" +
-  "<p>それで、そうしました。</p>" +
-  "<p>そして考えました：流れで任せたら、旅行はもっと楽しいんだ。</p>" +
-  "<p>それで、そうしました。</p>";
-
-const niigataText = "<p>It was green. Mountains. Grasses. Then it was dark.</p>" +
-  "<p>Suddenly, everything was white.</p>" +
-  "<p>Snow. Wonderful snow. It was beautiful.</p>" +
-  "<p>But oh, it's so cold.</p>" +
-  "<br>" +
-  "<p>緑。山。草。そして、暗くなった。</p>" +
-  "<p>突然に、全部が白くなった。</p>" +
-  "<p>雪。素敵な雪。きれいだった。</p>" +
-  "<p>でも、寒い。</p>";
-
-const tocIbaText = "<p>JAXA. Japan's counterpart to NASA.</p>" +
-  "<p>Space has always been interesting for me.</p>" +
-  "<p>And so, visiting JAXA made me see certain parts and procedures.</p>" +
-  "<p>I probably barely scratched the surface.</p>" +
-  "<br>" +
-  "<p>JAXA。日本のNASA。</p>" +
-  "<p>私にとって、宇宙にはずっと前から興味がある。</p>" +
-  "<p>だから、JAXAを見たら、いろんな部分と手続きを見ました。</p>" +
-  "<p>でも、まだまだでしょう。</p>";
-
-const naganoText = "<p>I first heard of Karuizawa from Terrace House.</p>" +
-  "<p>It looked interesting. And full of beautiful sceneries.</p>" +
-  "<p>Well, it looked fancy.</p>" +
-  "<p>And it's full of tourists.</p>" +
-  "<br>" +
-  "<p>軽井沢はテラスハウスから初めて聞いた。</p>" +
-  "<p>面白そうだった。そしてきれいな景色はいっぱい。</p>" +
-  "<p>ま〜、オシャレだったな。</p>" +
-  "<p>観光客も多い。</p>";
-
-const nagGifText = "<p>The way to Gifu was beautiful.</p>" +
-  "<p>Mountains.</p>" +
-  "<p>Towns between mountains. Trees within mountains. Rivers below mountains.</p>" +
-  "<p>All thanks to that wonderfully long train ride. Worth it.</p>" +
-  "<br>" +
-  "<p>岐阜への道はきれいだった。</p>" +
-  "<p>山。</p>" +
-  "<p>山の間の町。山の中の森。山の下の川。</p>" +
-  "<p>全部、素晴らしい、長い電車に乗った時間のおかげだ。最高。</p>";
-
-const gifFukText = "<p>A day ago, everything was green.</p>" +
-  "<p>This day, everything was white.</p>" +
-  "<p>Who knew it only took a night of heavy snowfall to turn everything upside down?</p>" +
-  "<p>Coming from a place without snow, that's cool.</p>" +
-  "<br>" +
-  "<p>昨日、全部は緑だった。</p>" +
-  "<p>今日、全部が白くなった。</p>" +
-  "<p>一晩だけの大きい降雪で全部が変わった。考えられなかった。</p>" +
-  "<p>雪のない国から来て、すごかった。</p>";
-
-const fukuiText = "<p>Fukui is not really a well-known place for tourists.</p>" +
-  "<p>How did I come to know it? Well that's a secret.</p>" +
-  "<p>Out of all my travels, my stay in Fukui has left the greatest impact.</p>" +
-  "<p>Oh, and there are dinosaurs.</p>" +
-  "<br>" +
-  "<p>福井は観光客にはあんまり有名じゃない。</p>" +
-  "<p>なぜ福井は知ってた？それは内緒だ。</p>" +
-  "<p>全体的には、福井の時間は一番印象に残ってる。</p>" +
-  "<p>あ〜、恐竜もある。</p>";
-
-const otwHirText = "<p>The way to Hiroshima was average.</p>" +
-  "<p>Beautiful sceneries. Of snow. And then the snow disappears.</p>" +
-  "<p>Inside the train, around Kyoto, I met a talkative old man.</p>" +
-  "<p>It was interesting, but it eventually got tiring.</p>" +
-  "<br>" +
-  "<p>広島への道は普通。</p>" +
-  "<p>きれいな景色。雪。そして、雪が無くなちゃった。</p>" +
-  "<p>電車の中に、京都のへんで、めっちゃ喋るおじいさんが出会った。</p>" +
-  "<p>面白いだった、でも疲れる。</p>";
-
-const hiroshimaText = "<p>めちゃくちゃ可愛い！</p>";
-
-const hirNarText = "<p>Stayed in a guesthouse in Nara for backpackers.</p>" +
-  "<p>There was this world map with pins all over.</p>" +
-  "<p>They were guests from all over the world.</p>" +
-  "<p>So far, I'm their third Filipino guest.</p>" +
-  "<br>" +
-  "<p>奈良でバックパッカーのためのゲストハウスで泊まりました。</p>" +
-  "<p>世界地図があった。いろんなピンが回りで刺さった。</p>" +
-  "<p>それは、世界からのお客さんだった。</p>" +
-  "<p>今まで、私は３番目のフィリピン人だ。</p>";
-
-const shiKyoText = "<p style='text-align: right; float: left; writing-mode: vertical-rl; text-orientation: upright;'>ニンニンニン</p>" +
-  "<p style='text-align: right; float: right; writing-mode: vertical-rl; text-orientation: upright;'>ニンニンニン</p>";
-
-const narOsaText = "<p>Nara was full of deers.</p>" +
-  "<p>Hornless deers.</p>" +
-  "<p>Not this one.</p>" +
-  "<p>Splendid.</p>" +
-  "<br>" +
-  "<p>奈良には鹿がいっぱいだ。</p>" +
-  "<p>角のない鹿。</p>" +
-  "<p>これは違う。</p>" +
-  "<p>素晴らしい。</p>";
-
-const aichiText = "<p>The last day.</p>" +
-  "<p>And so it ends.</p>" +
-  "<br>" +
-  "<p>最後の日。</p>" +
-  "<p>終わりました。</p>";
-
-const travelList = {
-  'li-travel-map': travelMapText,
-  'li-181224-niigata': niigataText,
-  'li-181225-toc-iba': tocIbaText,
-  'li-181226-nagano': naganoText,
-  'li-181227-nag-gif': nagGifText,
-  'li-181228-gif-fuk': gifFukText,
-  'li-181229-fukui': fukuiText,
-  'li-181230-otw-hir': otwHirText,
-  'li-181231-hiroshima': hiroshimaText,
-  'li-190101-hir-nar': hirNarText,
-  'li-190102-shi-kyo': shiKyoText,
-  'li-190103-nar-osa': narOsaText,
-  'li-190104-aichi': aichiText
-};
+let currentPicIdx = 0;
 
 let hoveredList = '';
 let hoveredListOldColor = '';
 let isShown = '';
 
+document.onkeydown = checkKey;
+
 function showImage(imgPath) {
+  // Resets pic index for parts with toggleable pics
+  currentPicIdx = 0;
+
   // This runs only the first highlight
-  const imgContainer = document.getElementById('img-highlight');
-  if (!imgContainer.style.display) {
-    imgContainer.style.display = 'block';
+  const img = document.getElementById('img-highlight');
+  if (!img.style.display) {
+    img.style.display = 'block';
   };
 
   // Return old hovered list back to its original color
@@ -146,24 +26,8 @@ function showImage(imgPath) {
   hoveredListOldColor = hoveredList.style['background-color'];
   hoveredList.style['background-color'] = 'grey';
 
-  const img = document.getElementById('img-highlight');
   img.style.backgroundImage = "url(" + imgPath + ")";
   img.style.color = "black";
-};
-
-function showImageText() {
-  const hoveredListText = travelList[hoveredList.id];
-
-  const imgTextDiv = document.getElementById('div-img-text');
-  if (isShown) {
-    isShown = '';
-    imgTextDiv.style.display = 'none';
-  } else {
-    isShown = true;
-    imgTextDiv.style.display = 'block';
-    const imgText = document.getElementById('img-text');
-    imgText.innerHTML = hoveredListText;
-  };
 };
 
 function hoverComingSoon() {
@@ -175,4 +39,19 @@ function hoverComingSoon() {
 
 const mouseLeaveComingSoon = function() {
   event.target.innerText = event.target.prevVal;
+};
+
+function checkKey(e) {
+  if (hoveredList && hoveredList !== '') {
+    // itoigawa2019
+    const hoveredListId = hoveredList.id;
+    if (imageCount[hoveredListId] && imageCount[hoveredListId] > 1) {
+      e = e || window.event;
+      if (e.keyCode == '37') { // left arrow
+        getPrevPic();
+      } else if (e.keyCode == '39') { // right arrow
+        getNextPic();
+      };
+    };
+  }
 };
